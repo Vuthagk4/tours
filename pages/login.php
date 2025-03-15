e81340e50e3d3cf697429e2475a6b449f953b413
Thany168 <ounthany5@gmail.com>
2025-03-15 16:36:31 +0700

    Login

--- pages/login.php
+++ pages/login.php
@@ -36,3 +36,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
+
+<form action="login.php" method="POST">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
